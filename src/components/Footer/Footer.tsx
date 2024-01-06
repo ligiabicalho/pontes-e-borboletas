571b049bcde4297c71e17e875ff20fb28e416722
Lígia <ligiabicalho@gmail.com>
2024-01-06 06:03:02 -0300

    feat: add Footer in HomePage

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export function Footer() {
+  return (
+    <div className="flex items-end justify-center w-full h-fit py-4">
+      <p className="text-[10px]">
+        Desenvolvido por{" "}
+        <a
+          href="https://www.linkedin.com/in/ligiabicalho/"
+          target="_blank"
+          className="text-[10px] underline hover:no-underline"
+        >
+          Lígia Bicalho
+        </a>
+        {/* , Jaider Nunes e Breno G */}
+      </p>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
+import { Footer } from "@/components/Footer/Footer";
+import { FAQ } from "@/components/Home/FAQ/FAQ";
 import Hero from "@/components/Home/Hero/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
+
+      <FAQ />
+
+      <Footer />
     </>
   );
 }
