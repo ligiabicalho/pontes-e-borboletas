e75edb33a5bdcf687e79854895a3450c7d1ac070
Lígia <ligiabicalho@gmail.com>
2024-01-06 01:21:01 -0300

    feat: create initial HomePage

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Calculator from "@/components/Home/Hero/Calculator";
+import DefaultLayout from "@/layouts/DefaultLayout";
+
+export default function Home() {
+  return (
+    <>
+      <DefaultLayout>
+        <Calculator />
+      </DefaultLayout>
+    </>
+  );
+}
