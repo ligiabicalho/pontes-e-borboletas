1ee3b394b05002a7e5995fd813d0a8ba994da161
Lígia <ligiabicalho@gmail.com>
2024-01-08 16:15:28 -0300

    feat: create Contact and AboutUs pages (under construction)

---
+++ src/pages/about-us.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export default function AboutUs() {
+  return (
+    <section className="lg:flex flex-col py-20 items-center justify-center">
+      <h2 className="font-primary text-2xl lg:text-4xl text-center mb-6">
+        Sobre o Instituto Pontes e Borboletas
+      </h2>
+
+      <p>Em construção.</p>
+    </section>
+  );
+}
---
+++ src/pages/contact.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export default function Contact() {
+  return (
+    <section className="lg:flex flex-col py-20 items-center justify-center">
+      <h2 className="font-primary text-2xl lg:text-4xl text-center mb-6">
+        Fale conosco!
+      </h2>
+
+      <p>Em construção.</p>
+    </section>
+  );
+}
