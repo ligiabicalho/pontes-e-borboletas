40224e3e1e6118a038380f7d1b050bfc33b41dd9
Lígia <ligiabicalho@gmail.com>
2024-01-08 16:13:54 -0300

    feat: update Default Layout

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,10 +1,12 @@
 import { Navbar } from "@/components/Navbar/Navbar";
+import { Footer } from "@/components/Footer/Footer";
 
 export default function DefaultLayout({ children }: React.PropsWithChildren) {
   return (
-    <div className="lg:pb-0 min-h-screen">
+    <div className="lg:pb-0 bg-gradient min-h-screen">
       <Navbar />
       {children}
+      <Footer />
     </div>
   );
 }
