faee8f4443e87ac584e7243332c7e74c0c3b678d
Lígia <ligiabicalho@gmail.com>
2024-01-08 20:06:26 -0300

    refactor: updates Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function Footer() {
   return (
-    <div className="flex items-end justify-center w-full h-fit py-4 absolute bottom-0">
+    <div className="text-center bottom-0 w-full p-4">
       <p className="text-[10px]">
         Desenvolvido por{" "}
         <a
