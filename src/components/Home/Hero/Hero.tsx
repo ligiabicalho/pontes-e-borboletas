1a1a97a1180465640cb62ddc0f62223f1d9cf4fb
Lígia <ligiabicalho@gmail.com>
2024-01-08 20:06:42 -0300

    refactor: updates Hero section

--- src/components/Home/Hero/Hero.tsx
+++ src/components/Home/Hero/Hero.tsx
@@ -1,9 +1,22 @@
+import CopyToClipboardButton from "@/components/Home/Hero/CopyToClipboardButton";
+import ShoppingList from "./ShoppingList";
 export default function Hero() {
   return (
-    <section className="flex flex-col p-8 pt-20 bg-gradient min-h-screen lg:flex-row lg:px-0">
-      <div className="container flex flex-col items-center justify-around lg:flex-row">
-        <h2>Feira Outra IPB</h2>
-
+    <section className="flex flex-col bg-gradient p-8 pt-20 min-h-screen lg:px-0">
+      <div className="flex flex-col items-center justify-around gap-y-4">
+        <h2 className="font-bold text-xl">Feira Outra IPB</h2>
+        <div className="self-start">
+          <p>Local: CroccoVeg </p>
+          <p>Dia: 11/01/2024</p>
+        </div>
+        <ShoppingList />
+        <p>Chave PIX - CNPJ: </p>
+        <div className="flex items-center gap-2">
+          <span id="pix-key" aria-label="Chave pix">
+            000000000000
+          </span>
+          <CopyToClipboardButton />
+        </div>
       </div>
     </section>
   );
