3e185c3dbe22592d0997d2701efaba8f8000879d
Lígia <ligiabicalho@gmail.com>
2024-01-08 22:21:04 -0300

    feat: includes 'success' variant in Toast component

--- src/components/ui/use-toast.ts
+++ src/components/ui/use-toast.ts
@@ -23,7 +23,7 @@ const actionTypes = {
 let count = 0;
 
 function genId() {
-  count = (count + 1) % Number.MAX_SAFE_INTEGER;
+  count = (count + 1) % Number.MAX_VALUE;
   return count.toString();
 }
 
