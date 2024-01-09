6c4dfe750df1eb121ffee87047b5beaa15efa0ba
Lígia <ligiabicalho@gmail.com>
2024-01-08 23:29:56 -0300

    style: add 'success' variant in toast in CopyToClipboardButton

--- src/components/Home/Hero/CopyToClipboardButton.tsx
+++ src/components/Home/Hero/CopyToClipboardButton.tsx
@@ -11,6 +11,7 @@ const CopyToClipboardButton = () => {
     clipboard.on("success", (e: any) => {
       console.log("Texto copiado para a área de transferência", e.text);
       toast({
+        variant: "success",
         title: "Sucesso!",
         description: "Chave PIX copiada para a área de transferência",
       });
@@ -21,6 +22,7 @@ const CopyToClipboardButton = () => {
     clipboard.on("error", () => {
       console.error("Erro ao copiar o texto para a área de transferência");
       toast({
+        variant: "destructive",
         title: "Erro ao copiar.",
         description:
           "O texto não foi copiado para a área de transferência, tente novamente.",
