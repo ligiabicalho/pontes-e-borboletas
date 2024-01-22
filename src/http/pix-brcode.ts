1da473178fc08647f5e18ae272768b8d91e4ae76
Lígia Bicalho <ligiabicalho@gmail.com>
2024-01-22 08:14:33 -0300

    fix: fixes the params to generate brcode

--- src/http/pix-brcode.ts
+++ src/http/pix-brcode.ts
@@ -3,6 +3,13 @@ type PixBrCodeResponse = {
   error: string;
 };
 
+const transformBrcode = (brcode: string) => {
+  let transformedCode = brcode.replace("br.gov.bcb.pix", "BR.GOV.BCB.PIX");
+  // para chave pix TELEFONE, substituir o '0114 ' por '0114+'
+  transformedCode = transformedCode.replace(/0114\s+/g, "0114+");
+  return transformedCode;
+};
+
 export const getPixBrCode = async (value: string) => {
   try {
     const apiUrl = `/api/pix-brcode?value=${value}`;
@@ -11,8 +18,8 @@ export const getPixBrCode = async (value: string) => {
     const { data, error }: PixBrCodeResponse = await response.json();
 
     if (error) throw new Error(error);
-
-    return data?.brcode;
+    const brcode = transformBrcode(data?.brcode);
+    return brcode;
   } catch (error: any) {
     console.error("Erro ao gerar Pix BrCode.", error.message);
     throw new Error("Erro ao gerar Pix BrCode.", error.message);
--- src/pages/api/pix-brcode.ts
+++ src/pages/api/pix-brcode.ts
@@ -17,32 +17,30 @@ export default async function handler(
   const { value } = req.query;
 
   const { name, city, output, pixKey, txid, mcc }: PixQrCodeQueries = {
-    name: "Lina Raquel de Oliveira Marinho",
+    name: "Lina Raquel de Oliveira M",
     city: "Belo Horizonte",
     output: "br", // gera br code
-    pixKey: "21997555322",
-    txid: "Feira Outra",
-    mcc: "5811", // DISTRIBUIÇÃO E PRODUÇÃO DE ALIMENTOS
+    pixKey: "+5521997555322",
+    txid: "FeiraOutra", // não pode ter espaços
+    // mcc: "7372",
   };
 
   try {
-    const apiUrl = `https://gerarqrcodepix.com.br/api/v1?nome=${name}&cidade=${city}&valor=${value}&saida=${output}&chave=${pixKey}&txid=${txid}&mcc=${mcc}`;
+    const apiUrl = `https://gerarqrcodepix.com.br/api/v1?nome=${name}&cidade=${city}&chave=${pixKey}&valor=${value}&saida=${output}&txid=${txid}&mcc=${mcc}`;
 
     const response = await fetch(apiUrl, {
       method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      redirect: "follow",
     });
 
     if (!response.ok) {
-      return res.status(500).json({ error: "Erro ao gerar Pix QrCode" });
+      return res.status(500).json({ error: "Erro ao gerar Pix BrCode" });
     }
 
     const data = await response.json();
     return res.status(200).json({ data });
   } catch (error: any) {
-    console.error("Erro ao gerar Pix QrCode:", error);
-    return res.status(500).json({ error: "Erro ao gerar Pix QrCode" });
+    console.error("Erro ao gerar Pix BrCode:", error);
+    return res.status(500).json({ error: "Erro ao gerar Pix BrCode" });
   }
 }
