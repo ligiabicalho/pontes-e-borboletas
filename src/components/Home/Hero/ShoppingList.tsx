eef563b2a8d84d2d9662f4f82c76bf5177cfc1dd
Lígia Bicalho <ligiabicalho@gmail.com>
2024-01-29 20:49:20 -0300

    feat: Add accordion component to ShoppingList and others updates

--- src/components/Home/Hero/ShoppingList.tsx
+++ src/components/Home/Hero/ShoppingList.tsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 import productsList from "../../../db/productsList.json";
 import GetPixCopyAndPaste from "./GetPixCopyAndPaste";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 type Product = {
   id: number;
@@ -15,12 +21,9 @@ type Product = {
 const ShoppingList = () => {
   // Seleciona só os items ativos
   const activeItems = productsList.filter((item) => item.active);
-  console.log("activeItems", activeItems);
-
 
   const sortProducts = (products: Product[]) => {
     return products.sort((a, b) => {
-
       //Mantém os itens checados na base da lista
       // if (a.checked && !b.checked) return 1;
       // if (!a.checked && b.checked) return -1;
@@ -38,7 +41,7 @@ const ShoppingList = () => {
 
   const handleCheck = (id: number) => {
     let updatedItems = [...items];
-    const [ itemChecked ] = updatedItems.filter((item) => item.id === id);
+    const [itemChecked] = updatedItems.filter((item) => item.id === id);
     itemChecked.checked = !itemChecked.checked;
 
     updatedItems = sortProducts(updatedItems);
@@ -80,59 +83,64 @@ const ShoppingList = () => {
         <div className="overflow-x-auto w-full">
           <table className="w-full">
             <tbody className="divide-y divide-gray-200">
-              {items
-                .filter((item) => item.active)
-                .map((item) => (
-                  <tr key={item.id}>
-                    <td
-                      className={`flex items-center px-4 py-2 whitespace-nowrap ${
-                        item.checked && "bg-gray-100"
-                      }`}
+              {items.map((item) => (
+                <tr key={item.id}>
+                  <td
+                    className={`flex items-center px-4 py-2 whitespace-nowrap ${
+                      item.checked && "bg-gray-100"
+                    }`}
+                  >
+                    <label
+                      htmlFor={`${item.id}`}
+                      className="flex flex-col w-full"
                     >
-                      <label htmlFor={`${item.id}`} className="flex flex-col w-full">
-                        <div className="flex">
-                          <input
-                            id={`${item.id}`}
-                            type="checkbox"
-                            checked={item.checked}
-                            onChange={() => handleCheck(item.id)}
-                            className="h-5 w-5 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-                            />
-                          <span className="ml-2 block text-sm font-medium text-gray-900">
-                            {item.name}
-                          </span>
-                        </div>
-                        <p className="flex px-2 ml-5 justify-between">
-                          <span className="whitespace-nowrap text-sm text-gray-500">
-                            {item.unit}
-                          </span>
-                          <span className="whitespace-nowrap text-sm text-gray-500">
-                            R${item.price.toFixed(2).split(".").join(",")}
-                          </span>
-                        </p>
-                      </label>
-                    </td>
-                        
-                  </tr>
-                ))}
+                      <div className="flex">
+                        <input
+                          id={`${item.id}`}
+                          type="checkbox"
+                          checked={item.checked}
+                          onChange={() => handleCheck(item.id)}
+                          className="h-5 w-5 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                        />
+                        <span className="ml-2 block text-sm font-medium text-gray-900">
+                          {item.name}
+                        </span>
+                      </div>
+                      <p className="flex px-2 ml-5 justify-between">
+                        <span className="whitespace-nowrap text-sm text-gray-500">
+                          {item.unit}
+                        </span>
+                        <span className="whitespace-nowrap text-sm text-gray-500">
+                          R${item.price.toFixed(2).split(".").join(",")}
+                        </span>
+                      </p>
+                    </label>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
-        <p className="my-2 italic self-start">
+      </div>
+
+      <div className="flex flex-col gap-3 lg:w-[25%]">
+        <p className="italic self-start">
           Sub-total: R${subTotalValue.toFixed(2)}
         </p>
         <ul className="flex flex-col pl-2 self-start">
-          {items.map((item) => (
-            item.checked && 
-            <li key={item.id} className="text-xs">
-              {`${item.name} ${item.unit} R$${item.price.toFixed(2).split(".").join(",")}`}
-            </li>
-        ))}
+          {items.map(
+            (item) =>
+              item.checked && (
+                <li key={item.id} className="text-xs">
+                  {`${item.name} ${item.unit} R$${item.price
+                    .toFixed(2)
+                    .split(".")
+                    .join(",")}`}
+                </li>
+              ),
+          )}
         </ul>
-      </div>
-
-      <div className="flex flex-col gap-4 lg:w-[25%]">
-        <fieldset className="flex flex-col gap-2 lg:gap-0">
+        <fieldset>
           <legend className="my-2">Contribuição:</legend>
           <div className="px-2">
             <label className="flex gap-1" htmlFor="rate-30">
@@ -164,21 +172,39 @@ const ShoppingList = () => {
           </div>
         </fieldset>
         <div className="py-2">
-          <p className="text-xs italic pb-2">
+          <p className="italic text-[16px] pb-2">
             Escolher contribuir ou não é livre porque a sua decisão é outra
             economia com a gente!
           </p>
-          <p className="text-xs">Transparência dos nossos custos:</p>
-          <p className="text-xs">
-            luz (R$290,00), água (R$220,00), internet e telefonia (R$170,00),
-            frete (R$450,00), mídias digitais (R$340,00), equipe - 01 Armazém;
-            07 feiras por semana; 05 dias de Cozinha para população em situação
-            de rua - (R$10.320,00)
-          </p>
+
+          <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+              <AccordionTrigger className="text-xs">
+                Transparência dos nossos custos:
+              </AccordionTrigger>
+              <AccordionContent>
+                <ul>
+                  <li className="text-xs">luz (R$290,00)</li>
+                  <li className="text-xs">água (R$220,00)</li>
+                  <li className="text-xs">internet e telefonia (R$170,00)</li>
+                  <li className="text-xs">frete (R$450,00)</li>
+                  <li className="text-xs">mídias digitais (R$340,00)</li>
+                  <li className="text-xs">
+                    equipe: 01 Armazém, 07 feiras por semana, 05 dias de Cozinha
+                    para população em situação de rua - (R$10.320,00)
+                  </li>
+                </ul>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
         </div>
         <p className="my-2 font-bold">
           Total a pagar:{" "}
-          {!!subTotalValue && `R$${totalToPay.split(".").join(",")}`}
+          {!subTotalValue ? (
+            <span className="text-md font-thin italic">Adicione items</span>
+          ) : (
+            `R$${totalToPay.split(".").join(",")}`
+          )}
         </p>
       </div>
       <div className="lg:w-[28%]">
