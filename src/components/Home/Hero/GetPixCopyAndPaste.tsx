589e6bb8b6aeddacc2585d7abe728b3e1e499963
Lígia Bicalho <ligiabicalho@gmail.com>
2024-01-29 09:58:51 -0300

    refactor: update GetPixCopyAndPaste component

--- src/components/Home/Hero/GetPixCopyAndPaste.tsx
+++ src/components/Home/Hero/GetPixCopyAndPaste.tsx
@@ -3,13 +3,15 @@ import { getPixBrCode } from "../../../http/pix-brcode";
 import { Button } from "../../ui/button";
 import { useToast } from "@/components/ui/use-toast";
 import { useMutation } from "@tanstack/react-query";
+import { Copy } from "lucide-react";
 
-const GetPixCopyAndPaste = ({ value }: { value: string }) => {
+const GetPixCopyAndPaste = ({ value }: { value: number }) => {
+  console.log("value", value);
   const { toast } = useToast();
   const targetId = "pix-brcode-copy-and-paste";
 
   const {
-    data: pixBrCode,
+    data,
     isLoading,
     isSuccess,
     mutate: getPixBrCodeMutate,
@@ -29,7 +31,7 @@ const GetPixCopyAndPaste = ({ value }: { value: string }) => {
   });
 
   const handleClickGetPixQrCode = () => {
-    if (value === "0") {
+    if (!value || value === 0) {
       toast({
         variant: "destructive",
         title: "Nenhum produto selecionado.",
@@ -43,10 +45,11 @@ const GetPixCopyAndPaste = ({ value }: { value: string }) => {
 
   return (
     <div className="lg:flex flex flex-col justify-center items-center">
-      <p className="flex lg:mr-2">
-        Pague sua compra via Pix: gere o código, em seguida o copie e cole no
-        app do seu banco.
+      <p className="lg:mr-2">
+        Pague sua compra via <span className="font-bold">Pix Copia e Cola</span>: gere o código, em seguida o copie <span><Copy size={14} className="inline mx-1"/></span> e cole no
+        app do seu banco. 
       </p>
+        
       <Button
         aria-label="Gerar QrCode copia e cola"
         className="bg-purple-700 text-yellow-300 p-4 rounded-sm shadow-md shadow-purple-400 hover:bg-purple-600 focus:ring-2 focus:ring-yellow-400 focus:ring-offset-2 m-4"
@@ -55,21 +58,21 @@ const GetPixCopyAndPaste = ({ value }: { value: string }) => {
       >
         Gerar QrCode copia e cola
       </Button>
-      {isSuccess && pixBrCode && (
+      {isSuccess && data && (
         <div className="flex flex-row items-center gap-2">
           <div className="flex flex-row bg-background p-2 rounded-md text-xs">
             <p className="flex overflow-x-scroll lg:overflow-hidden">
               <span
                 id={targetId}
                 aria-label="Pix QrCode copia e cola"
-                className="w-[260px] lg:w-[300px] max-h-[18px]"
+                className="w-[220px] lg:w-[300px] max-h-[18px]"
               >
-                {pixBrCode}
+                {data.brCode}
               </span>
             </p>
             <p>...</p>
           </div>
-          <CopyToClipboardButton text={pixBrCode} targetId={`#${targetId}`} />
+          <CopyToClipboardButton text={data.brCode} targetId={`#${targetId}`} />
         </div>
       )}
     </div>
