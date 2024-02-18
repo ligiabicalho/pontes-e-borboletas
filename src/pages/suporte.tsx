dc9f0b191c7e69935e82ea8ef071275c94e4675f
Lígia Bicalho <ligiabicalho@gmail.com>
2024-02-18 08:04:14 -0300

    feat: update suporte page with feedback form

--- src/pages/suporte.tsx
+++ src/pages/suporte.tsx
@@ -5,10 +5,21 @@ export default function Support() {
   return (
     <section className="flex flex-col pt-20 px-6">
       <h2 className="font-primary text-2xl lg:text-4xl text-center mb-6">
-        Instituto Pontes e Borboletas
+        Suporte
       </h2>
-
-      <p>Em construção.</p>
+      <p>
+        Colabore com as melhorias do App, deixe seu feedback ou reporte algum
+        erro encontrado ao utilizá-lo.
+      </p>
+      <div className="flex justify-center">
+        <iframe
+          src="https://docs.google.com/forms/d/e/1FAIpQLSehD5vJa2aKmpnuMuChRiq0z5bAllsn7TjREUCokfHZ-_cSQg/viewform?embedded=true"
+          width="640"
+          height="1365"
+        >
+          Carregando…
+        </iframe>
+      </div>
     </section>
   );
 }
