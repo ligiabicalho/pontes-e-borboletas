736f1d6a7f9cc5994d718f89a0d348e3f54023a1
Lígia Bicalho <ligiabicalho@gmail.com>
2024-03-25 00:54:40 -0300

    feat: Add CheckoutBottomBar component

---
+++ src/components/BottomBar/CheckoutBottomBar.tsx
@@ -0,0 +1,42 @@
+import { Button } from "../ui/button";
+import { BottomBar } from "./BottomBar";
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+type CheckoutProps = {
+  subTotalValue: number;
+  totalValue: number;
+  hasPixCode: boolean;
+};
+
+export const CheckoutBottomBar = ({
+  subTotalValue,
+  totalValue,
+  hasPixCode,
+}: CheckoutProps) => {
+  const router = useRouter();
+  const currentPage = router.pathname;
+  const targetPage = `${currentPage}#checkout`;
+  const value = hasPixCode ? totalValue : subTotalValue;
+
+  return (
+    <BottomBar>
+      <div className="flex items-start space-x-2">
+        <p className="font-primary text-neutral-black text-right text-xs">
+          {hasPixCode ? "Total" : "Sub-total"}
+        </p>
+
+        <p className="flex text-secondary font-primary">
+          <span className="text-xs">R$</span>
+          <span className="text-2xl pl-1">
+            {value.toFixed(2).split(".").join(",")}
+          </span>
+        </p>
+      </div>
+
+      <Link href={targetPage}>
+        <Button>Finalizar compra</Button>
+      </Link>
+    </BottomBar>
+  );
+};
