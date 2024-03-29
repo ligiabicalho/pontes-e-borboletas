168fcf856000b8b891721facffcf14860cc1b816
Lígia Bicalho <ligiabicalho@gmail.com>
2024-03-29 20:06:09 -0300

    fix: Fix search bar clear button size

--- src/components/Home/ShoppingList/SearchBar.tsx
+++ src/components/Home/ShoppingList/SearchBar.tsx
@@ -20,55 +20,26 @@ const SearchBar: React.FC<SearchBarProps> = ({
       <Label className="relative flex py-1 text-sm w-full" htmlFor="search">
         <Input
           id="search"
-          type="search"
+          type="text"
           placeholder="Pesquisar"
           value={searchQuery}
           onChange={(e: any) => handleSearch(e.target.value)}
           className="border mb-2 pl-2"
         />
-        <div className="absolute right-2 top-1/2 transform -translate-y-3 px-1">
+        <div className="absolute right-1 top-5 transform -translate-y-3 flex h-8 w-8 items-center justify-center">
           {isInputEmpty ? (
             <Search size={18} color="gray" />
           ) : (
             <X
               cursor="pointer"
-              size={18}
+              size={36}
               color="gray"
+              className="p-2"
               onClick={handleClearSearch}
             />
           )}
         </div>
       </Label>
-      {/* TODO //FIXME: Implementar busca por categoria */}
-      {/* <div className="flex gap-4">
-        <Label
-          className="flex items-center gap-1 py-1 text-sm"
-          htmlFor="name"
-        >
-          <Input
-            type="radio"
-            id="name"
-            name="searchType"
-            value="name"
-            onChange={() => setSearchType("name")}
-          />
-          Nome
-        </Label>
-
-        <Label
-          className="flex items-center gap-1 py-1 text-sm"
-          htmlFor="category"
-        >
-          <Input
-            type="radio"
-            id="category"
-            name="searchType"
-            value="category"
-            onChange={() => setSearchType("category")}
-          />
-          Categoria
-        </Label>
-      </div> */}
     </div>
   );
 };
