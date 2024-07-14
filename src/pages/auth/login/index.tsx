664f6f67f6c419bd3680465a180876dd06d4e6fd
Lígia Bicalho <ligiabicalho@gmail.com>
2024-07-14 12:50:58 -0300

    feat: Add login and signup pages

---
+++ src/pages/auth/login/index.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "@/components/auth/loginForm";
+
+export default function LoginPage() {
+  return (
+    <div className="flex lg:h-full items-center px-2 container relative">
+      <LoginForm />
+    </div>
+  );
+}
---
+++ src/pages/auth/signup/index.tsx
@@ -0,0 +1,9 @@
+import SignUpForm from "@/components/auth/signUpForm";
+
+export default function SignUpPage() {
+  return (
+    <div className="flex lg:h-full items-center px-2 container relative">
+      <SignUpForm />
+    </div>
+  );
+}
