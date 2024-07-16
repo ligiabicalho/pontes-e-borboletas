3166c2ba8300675991205199f8d57cdddf866caa
Lígia Bicalho <ligiabicalho@gmail.com>
2024-07-16 14:02:07 -0300

    feat: Update signup and login pages with new design and layout

--- src/components/auth/signUpForm.tsx
+++ src/components/auth/signUpForm.tsx
@@ -11,7 +11,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-export function SignUpForm() {
+export default function SignUpForm() {
   return (
     <Card className="mx-auto max-w-sm">
       <CardHeader>
--- src/pages/auth/signup/index.tsx
+++ src/pages/auth/signup/index.tsx
@@ -1,9 +1,12 @@
 import SignUpForm from "@/components/auth/signUpForm";
+import { ThemeProvider } from "@/providers/admin-panel/theme-provider";
 
 export default function SignUpPage() {
   return (
-    <div className="flex lg:h-full items-center px-2 container relative">
-      <SignUpForm />
-    </div>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <div className="flex lg:h-full items-center px-2 container relative">
+        <SignUpForm />
+      </div>
+    </ThemeProvider>
   );
 }
